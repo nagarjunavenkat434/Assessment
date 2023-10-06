8a6af50a274daba9531e7ea175ccd6d5d6636e36
venkata nagarjuna <venkata@bassure.com>
2023-10-06 16:18:06 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,25 @@
 
-function userFunction(){
-    return(
-        context.log("helloworld")
-    )
-}
 
-userFunction();
-// const express = require('express');
-// const axios = require('axios');
-// const cors = require('cors');
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
 
-// const app = express();
-// app.use(cors());
-// const { Client, Databases, ID } = require("appwrite");
-// const sdk = require("node-appwrite");
+const app = express();
+app.use(cors());
+const { Client, Databases, ID } = require("appwrite");
+const sdk = require("node-appwrite");
 
-// //appwrite configuration
-// const client = new sdk.Client()
+//appwrite configuration
+const client = new sdk.Client()
 
-// client.setEndpoint('https://cloud.appwrite.io/v1')
-//     .setProject('651ef9776046ee84cb9f')
-//     .setKey("91713a6f558fc3a157a99c594d1d935c709a0cc35d124e9d746019d43ea9aa85f95432857e7da1dcf106af1cd6369be0e7f397170e7a35ae8d1e7f61ce593491acb3c1c8ac1cb7db8b6ed0edac6e171b0886248a1c0cf1b80d452708e39e465bb5d56b8f06d0c832cbdc5842779e59c064b6ae524c4d4239625f5c1235e81c35");
+client.setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('651ef9776046ee84cb9f')
+    .setKey("91713a6f558fc3a157a99c594d1d935c709a0cc35d124e9d746019d43ea9aa85f95432857e7da1dcf106af1cd6369be0e7f397170e7a35ae8d1e7f61ce593491acb3c1c8ac1cb7db8b6ed0edac6e171b0886248a1c0cf1b80d452708e39e465bb5d56b8f06d0c832cbdc5842779e59c064b6ae524c4d4239625f5c1235e81c35");
 
-// const databases = new sdk.Databases(client);
+const databases = new sdk.Databases(client);
 
-// //fetching data from rapid api
+//fetching data from rapid api
+
 // const options = {
 //     method: 'GET',
 //     url: 'https://amazonlive.p.rapidapi.com/product',
@@ -37,7 +32,9 @@ userFunction();
 //         'X-RapidAPI-Host': 'amazonlive.p.rapidapi.com'
 //     }
 // };
-// //creating document in collection
+
+//creating document in collection
+
 // app.post('/api/storeproducts', async (req, res) => {
 //     try {
 //         const response = await axios.request(options);
@@ -55,8 +52,9 @@ userFunction();
 //     }
 // });
 
-// //fetching data from database
-// async function userFunction() {
+//fetching data from database
+
+async function userFunction() {
 // app.get('/api/storeproductsdata', async (req, res) => {
 //     try {
 
@@ -76,8 +74,17 @@ userFunction();
 // });
 // }
 
-// userFunction();
-// const port = 3001;
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
+context.log('hello world');}
+
+userFunction()
+.then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
